195dd716415d9a54dbf961ddf08223301d7afa6d
Rikard Teodorsson <rikardt@student.chalmers.se>
2018-11-23 15:23:37 +0100

    Updated mission

--- mdsd/src/mdsd/model/Mission.java
+++ mdsd/src/mdsd/model/Mission.java
@@ -6,6 +6,12 @@ public class Mission {
     private final Point[] points;
     private int currentHeading;
 
+    /**
+     * Creates a new mission from an array of points
+     *
+     * @param points the points to be visited
+     * @throws IllegalArgumentException if the argument is null or empty
+     */
     public Mission(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException("Points can't be null");
@@ -16,6 +22,11 @@ public class Mission {
         currentHeading = 0;
     }
 
+    /**
+     * Returns the next point in the mission. If the mission is done it returns null
+     *
+     * @return the next Point to visit, or null if end of list
+     */
     public Point getNextPoint() {
         if (currentHeading >= points.length) {
             return null;
@@ -23,7 +34,12 @@ public class Mission {
         return points[currentHeading++];
     }
 
-    public int getCurrentHeading() {
-        return currentHeading;
+    /**
+     * Returns the current point
+     *
+     * @return the point being visited
+     */
+    public Point getCurrentPoint() {
+        return points[currentHeading];
     }
 }
