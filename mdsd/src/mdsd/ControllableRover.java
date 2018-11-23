78ef2471ff0a6dfb3ed905fdf840bdaf95278fe1
Pontus Eriksson <pontus_eriksson@live.com>
2018-11-23 15:40:35 +0100

    Created ControllableRover interface, and made Robot implement it with method stubs.

---
+++ mdsd/src/mdsd/ControllableRover.java
@@ -0,0 +1,51 @@
+package mdsd;
+
+import project.Point;
+
+/**
+ * A rover that can be controlled by an operator.
+ * @author bondi
+ *
+ */
+public interface ControllableRover {
+	
+	/*
+	 * Set a new mission for the rover to execute.
+	 */
+	public void setMission(Mission mission);
+	
+	/*
+	 * Get the current mission of the rover.
+	 */
+	public Mission getMission();
+	
+	/*
+	 * Get the current position for the rover.
+	 */
+	public Point getPosition();
+	
+	/*
+	 * Get the current status of the rover.
+	 */
+	public Status getStatus();
+	
+	/*
+	 * Start executing the current mission, if there is any.
+	 */
+	public void start();
+	
+	/*
+	 * Stop whatever the rover is doing.
+	 */
+	public void stop();
+	
+	/*
+	 * Get a description of all faults of the rover, if any.
+	 */
+	public String[] getFaults();
+	
+	/*
+	 * Check if the rover is faulty or not.
+	 */
+	public Boolean isFaulty();
+}
--- mdsd/src/mdsd/Robot.java
+++ mdsd/src/mdsd/Robot.java
@@ -3,7 +3,7 @@ package mdsd;
 import project.AbstractRobotSimulator;
 import project.Point;
 
-public class Robot extends AbstractRobotSimulator {
+public class Robot extends AbstractRobotSimulator implements ControllableRover {
 
 	
 	
@@ -15,6 +15,54 @@ public class Robot extends AbstractRobotSimulator {
 	@Override
 	public String toString() {
 		return "Robot " + this.getName();
+	}
+
+	@Override
+	public void setMission(Mission mission) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mission getMission() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Status getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String[] getFaults() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean isFaulty() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
