ed1ef1618e7ae0ae67e0f7daf324c0c74f45188e
Pontus Eriksson <pontus_eriksson@live.com>
2018-12-12 11:50:18 +0100

    Added optional code for treating objects Simbad-style

--- mdsd/src/mdsd/model/Obstacle.java
+++ mdsd/src/mdsd/model/Obstacle.java
@@ -3,17 +3,21 @@ package mdsd.model;
 public class Obstacle {
     public float x;
     public float y;
+    //public float xy2;  // Stop coord in x or y, depending on horizontal
     public float length;  // Positive length in x or y, depending on horizontal
     public boolean horizontal;  // If not horizontal, vertical
-    
+
+    //public Obstacle(float x, float y, float xy2, boolean horizontal) {
     public Obstacle(float x, float y, float length, boolean horizontal) {
         this.x = x;
         this.y = y;
+        //this.xy2 = xy2;
         this.length = length;
         this.horizontal = horizontal;
     }
     
     public Obstacle(Obstacle ob) {
+        //this(ob.x, ob.y, ob.xy2, ob.horizontal);
         this(ob.x, ob.y, ob.length, ob.horizontal);
     }
 }
