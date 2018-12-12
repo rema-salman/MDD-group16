b1bdb1d24e75c9f2a812abcf05cf0370f80c2915
Pontus Eriksson <pontus_eriksson@live.com>
2018-12-12 15:38:17 +0100

    Areas now own their reward, changed Shape[] -> List<Shape> in Area

--- mdsd/src/mdsd/model/Area.java
+++ mdsd/src/mdsd/model/Area.java
@@ -2,6 +2,7 @@ package mdsd.model;
 
 import java.awt.Polygon;
 import java.awt.Shape;
+import java.util.List;
 import java.util.Set;
 
 import javax.vecmath.Point2f;
@@ -14,25 +15,38 @@ public class Area extends Polygon {
      * inside any of these shapes then it is considered to be inside of the area,
      * unless it is also inside one of the antiShapes
      */
-    private Shape[] shapes;
+    protected List<Shape> shapes;
+
     /**
      * These shapes are subtracted from the area for the purposes of collision
      * detection, if a point is inside any of the normal shapes but also inside an
      * anti shape then it is considered to not be inside of the area.
      */
-    private Shape[] antiShapes;
+    protected List<Shape> antiShapes;
     Set<Point2f> areaShapes;
-    
-    public Area(Shape[] shapes, Shape[] antiShapes) {
+
+    private int reward;
+
+    public Area(List<Shape> shapes, List<Shape> antiShapes, int reward) {
         this.shapes = shapes;
         this.antiShapes = antiShapes;
+        this.reward = reward;
     }
 
-    public Area(Set<Point2f> areaShapes) {
+    public Area(List<Shape> shapes, List<Shape> antiShapes) {
+        this(shapes, antiShapes, 0);
+    }
+
+    public Area(Set<Point2f> areaShapes, int reward) {
         this.setShapes(areaShapes);
         for(Point2f p : areaShapes) {
             this.addPoint((int)(p.getX()), (int)(p.getY()));
-        }     
+        }
+        this.reward = reward;
+    }
+
+    public Area(Set<Point2f> areaShapes) {
+        this(areaShapes, 0);
     }
     
     public void setShapes(Set<Point2f> areaShapes) {
@@ -40,31 +54,37 @@ public class Area extends Polygon {
     }
 
     /**
-     * Checks if a given point, p, is inside the area. This is true if the point is
-     * NOT inside any of the anti shapes, but is inside one of the normal shapes.
+     * Checks if a given point is inside the area. This is true if the point is
+     * inside the area but NOT inside any of the anti shapes.
      *
      * @param p The point which is being investigated.
      * @return True if the point is inside the area.
      */
     public boolean contains(Point2f p) {
-        // if inside any antiShape: return false
-        // if inside any shape: return true
-        // return false
+        float x = p.getX();
+        float y = p.getY();
         for (Shape shape : shapes) {
-            if (shape.contains(p.getX(), p.getY())) {
+            if (shape.contains(x, y)) {
+                for (Shape antiShape : antiShapes) {
+                    if (antiShape.contains(x, y)) {
+                        return false;
+                    }
+                }
                 return true;
             }
         }
         return false;
     }
 
-    // TODO: Check if actually deep copy
-    public Shape[] getShapes() {
-        return shapes.clone();
+    public List<Shape> getShapes() {
+        return shapes;
     }
 
-    // TODO: Check if actually deep copy
-    public Shape[] getAntiShapes() {
-        return antiShapes.clone();
+    public List<Shape> getAntiShapes() {
+        return antiShapes;
+    }
+
+    public int getReward() {
+        return reward;
     }
 }
