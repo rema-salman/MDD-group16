f04b5a3e28967528dbb113d0bb79c126c7c519f2
Pontus Eriksson <pontus_eriksson@live.com>
2018-12-13 11:47:17 +0100

    Division is no longer an Area

--- mdsd/src/mdsd/model/Division.java
+++ mdsd/src/mdsd/model/Division.java
@@ -5,19 +5,15 @@ import java.util.List;
 import java.util.Set;
 import javax.vecmath.Point2f;
 
-@SuppressWarnings("serial")
-public class Division extends Area {
+public class Division {
     public List<Area> rooms;
 
     public Division(Set<Point2f> shapes) {
-        super(shapes);
         rooms = new ArrayList<>();
         rooms.add(new Area(shapes));
     }
 
     public void addRoom(Area room) {
-        shapes.addAll(room.getShapes());
-        antiShapes.addAll(room.getAntiShapes());
         rooms.add(room);
     }
 }
