d7f25af4886c012dba3b79712379c1d6ef3fb5a5
Rikard Teodorsson <rikardt@student.chalmers.se>
2018-12-13 12:30:11 +0100

    Moved rover.start() to finally block

--- mdsd/src/mdsd/controller/MainController.java
+++ mdsd/src/mdsd/controller/MainController.java
@@ -3,127 +3,127 @@ package mdsd.controller;
 import mdsd.model.Area;
 import mdsd.model.Environment;
 
+import javax.vecmath.Point2f;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.vecmath.Point2f;
-
 public class MainController implements Observer {
-	private Set<IControllableRover> rovers;
-	private Environment environment;
-	private ScoreCalculator scoreCalculator;
-	private static MainController mainController = null;
+    private Set<IControllableRover> rovers;
+    private Environment environment;
+    private ScoreCalculator scoreCalculator;
+    private static MainController mainController = null;
 
-	private MainController() {
-		this.rovers = new HashSet<IControllableRover>();
-		this.environment = new Environment();
-	}
+    private MainController() {
+        this.rovers = new HashSet<IControllableRover>();
+        this.environment = new Environment();
+    }
 
-	public static MainController getInstance() {
-		if (mainController == null) {
-			mainController = new MainController();
-		}
-		return mainController;
-	}
+    public static MainController getInstance() {
+        if (mainController == null) {
+            mainController = new MainController();
+        }
+        return mainController;
+    }
 
-	/**
-	 * Main loop for the MainController.
-	 */
-	public void loopForever() {
-		try {
+    /**
+     * Main loop for the MainController.
+     */
+    public void loopForever() {
+        try {
 
-			while (true) {
-				// Update rover areas
-				for (IControllableRover rover : rovers) {
-					Point2f roverPos = rover.getJavaPosition();
-					if (rover.getRoom() == null || !rover.getRoom().contains(roverPos)) {
-						Runnable roverUpdate = () -> {
+            while (true) {
+                // Update rover areas
+                for (IControllableRover rover : rovers) {
+                    Point2f roverPos = rover.getJavaPosition();
+                    if (rover.getRoom() == null || !rover.getRoom().contains(roverPos)) {
+                        Runnable roverUpdate = () -> {
 
-							for (Area room : environment.getPhysicalAreas()) {
-								if (room.contains(roverPos)) {
-									rover.stop();
-									rover.setRoom(room);
-									try {
-										Thread.sleep(2000);
-									} catch (InterruptedException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-									rover.start();
-									break;
-								}
-							}
+                            for (Area room : environment.getPhysicalAreas()) {
+                                if (room.contains(roverPos)) {
+                                    rover.stop();
+                                    rover.setRoom(room);
+                                    try {
+                                        Thread.sleep(2000);
+                                    } catch (InterruptedException e) {
+                                        // TODO Auto-generated catch block
+                                        e.printStackTrace();
+                                    } finally {
+                                        rover.start();
+                                    }
+                                    break;
+                                }
+                            }
 
-						};
+                        };
 
-						Thread roverUpdateThread = new Thread(roverUpdate);
-						roverUpdateThread.start();
-					}
-				}
-				Thread.sleep(16);
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+                        Thread roverUpdateThread = new Thread(roverUpdate);
+                        roverUpdateThread.start();
+                    }
+                }
+                Thread.sleep(16);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void addRovers(Set<IControllableRover> rovers) {
-		this.rovers.addAll(rovers);
-	}
+    public void addRovers(Set<IControllableRover> rovers) {
+        this.rovers.addAll(rovers);
+    }
 
-	// TODO
-	public void getAllRoverPositions() {
+    // TODO
+    public void getAllRoverPositions() {
 
-	}
+    }
 
-	// TODO
-	public void getRoverPositions(Area area) {
+    // TODO
+    public void getRoverPositions(Area area) {
 
-	}
+    }
 
-	public int getScore() {
-		return (scoreCalculator == null ? 0 : scoreCalculator.getScore());
-	}
+    public int getScore() {
+        return (scoreCalculator == null ? 0 : scoreCalculator.getScore());
+    }
 
-	public Environment getEnvironment() {
-		return environment;
-	}
+    public Environment getEnvironment() {
+        return environment;
+    }
 
-	// TODO, return type
-	public void getRoverStatus() {
+    // TODO, return type
+    public void getRoverStatus() {
 
-	}
+    }
 
-	public List<IControllableRover> getRoverList() {
-		List<IControllableRover> list = new ArrayList<>();
-		list.addAll(rovers);
-		return list;
-	}
+    public List<IControllableRover> getRoverList() {
+        List<IControllableRover> list = new ArrayList<>();
+        list.addAll(rovers);
+        return list;
+    }
 
-	public void stopRover(IControllableRover rover) {
-		rover.stop();
-	}
+    public void stopRover(IControllableRover rover) {
+        rover.stop();
+    }
 
-	public void stop() {
-		for (IControllableRover r : rovers) {
-			r.stop();
-		}
-	}
+    public void stop() {
+        for (IControllableRover r : rovers) {
+            r.stop();
+        }
+    }
 
-	public void start() {
-		for (IControllableRover r : rovers) {
-			r.start();
-		}
-	}
+    public void start() {
+        for (IControllableRover r : rovers) {
+            r.start();
+        }
+    }
 
-	@Override
-	public void receiveEvent(Object event) {
-		// TODO
-	}
+    @Override
+    public void receiveEvent(Object event) {
+        // TODO
+    }
 
-	public void setScoreCalculator(ScoreCalculator sc) {
-		this.scoreCalculator = sc;
-	}
+    public void setScoreCalculator(ScoreCalculator sc) {
+        this.scoreCalculator = sc;
+    }
 }
